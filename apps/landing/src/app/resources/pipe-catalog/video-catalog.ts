25abc11d16f3191f040e681dfaa63eab0c3fcd9a
Florian Martens <florian_martens@outlook.de>
2025-09-05 16:38:19 +0200

    feat: Add videos for more pipes

--- apps/landing/src/app/resources/pipe-catalog/video-catalog.ts
+++ apps/landing/src/app/resources/pipe-catalog/video-catalog.ts
@@ -3,4 +3,5 @@ import { PipeId } from "@pipe0/client-sdk";
 export const videoCatalog = {
   "people:workemail:waterfall@1": "https://youtu.be/HFlm8XOIMsQ",
   "people:phone:profile:waterfall@1": "https://youtu.be/ZUa2ltX805A",
+  "message:send:slack@1": "https://youtu.be/GpRK8J-n8xg",
 } satisfies Partial<Record<PipeId, string>>;
