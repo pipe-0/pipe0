f4eb14103294a3e6b796bb2a03239261529d200f
Florian Martens <florian_martens@outlook.de>
2025-09-05 15:34:03 +0200

    fix: pipe docs paths

--- apps/landing/src/app/resources/pipe-catalog/video-catalog.ts
+++ apps/landing/src/app/resources/pipe-catalog/video-catalog.ts
@@ -1,3 +1,6 @@
 import { PipeId } from "@pipe0/client-sdk";
 
-export const videoCatalog = {} satisfies Partial<Record<PipeId, string>>;
+export const videoCatalog = {
+  "people:workemail:waterfall@1": "https://youtu.be/HFlm8XOIMsQ",
+  "people:phone:profile:waterfall@1": "https://youtu.be/ZUa2ltX805A",
+} satisfies Partial<Record<PipeId, string>>;
